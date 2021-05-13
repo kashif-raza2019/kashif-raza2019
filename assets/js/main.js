666b31917209c2d5330b7389faf40138d54095b9
Kashif Raza <kashifraza08012001@gmail.com>
2021-05-13 14:14:50 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: ComingSoon - v2.2.1
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
